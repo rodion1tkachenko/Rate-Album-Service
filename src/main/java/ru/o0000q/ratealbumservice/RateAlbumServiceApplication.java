982d5b75e6a64ba335296bc4e3a3a08918a6a00f
Rodion Tkachenko <rodion.tkachenko31@gmail.com>
2024-01-02 14:37:22 +0300

    Initial commit

---
+++ src/main/java/ru/o0000q/ratealbumservice/RateAlbumServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.o0000q.ratealbumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateAlbumServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateAlbumServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/o0000q/ratealbumservice/RateAlbumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.o0000q.ratealbumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateAlbumServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
