d8d5ecc900cc451e72fe1476aae328e660906c5f
Rodion Tkachenko <rodion.tkachenko31@gmail.com>
2024-01-16 18:38:52 +0300

    fill_database.sql edited: each foreign key uses an inner sql query instead of hard coding

--- src/test/resources/sql/fill_database.sql
+++ src/test/resources/sql/fill_database.sql
@@ -1,19 +1,19 @@
-INSERT INTO singer (name)
-VALUES ('Singer1'),
-       ('Singer2');
+INSERT INTO singer (id, name)
+VALUES (1, 'Singer1'),
+       (2, 'Singer2');
+SELECT SETVAL('singer_id_seq', (SELECT MAX(id) FROM singer));
 
--- Вставка тестовых данных в таблицу users
-INSERT INTO users (login, password, nickname, role)
-VALUES ('user1', 'password1', 'nickname1', 'role1'),
-       ('user2', 'password2', 'nickname2', 'role2');
+INSERT INTO users (id, login, password, nickname, role)
+VALUES (1, 'user1', 'password1', 'nickname1', 'role1'),
+       (2, 'user2', 'password2', 'nickname2', 'role2');
+SELECT SETVAL('users_id_seq', (SELECT MAX(id) FROM users));
 
+INSERT INTO album (id, title, singer_id, genre)
+VALUES (1, 'Album1', (select id from singer where name='Singer1'), 'Genre1'),
+       (2, 'Album2', (select id from singer where name='Singer1'), 'Genre2');
+SELECT SETVAL('album_id_seq', (SELECT MAX(id) FROM album));
 
--- Вставка тестовых данных в таблицу album
-INSERT INTO album (title, singer_id, genre)
-VALUES ('Album1', 1, 'Genre1'),
-       ('Album2', 2, 'Genre2');
-
--- Вставка тестовых данных в таблицу users_rating
-INSERT INTO users_rating (user_id, album_id, rate)
-VALUES (1, 1, 5),
-       (2, 2, 8);
+INSERT INTO users_rating (id, user_id, album_id, rate)
+VALUES (1, (select id from users where nickname='nickname1'), (select id from album where title='title1'), 5),
+       (2, (select id from users where nickname='nickname2'),  (select id from album where title='title2'), 8);
+SELECT SETVAL('users_rating_id_seq', (SELECT MAX(id) FROM users_rating));
